48da4bcb6bcbcc88ede170e3b952f0b95f774e69
lubenard <lubenard@student42.fr>
2021-03-26 14:24:59 +0100

    Finished Assignement_09

--- Assignement_09/main.c
+++ Assignement_09/main.c
@@ -2,8 +2,8 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/mount.h>
-#include <linux/fs.h>
 #include <linux/fs_struct.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 
@@ -14,8 +14,6 @@ static ssize_t read_module(struct file *file, char __user *user, size_t len, lof
 static const struct file_operations proc_file_fops = {
 	.read  = read_module,
 };
-
-//char *buffer;
 
 size_t get_max_mount_size(size_t *number_of_entrys)
 {
@@ -31,40 +29,47 @@ size_t get_max_mount_size(size_t *number_of_entrys)
 
 	list_for_each_entry(curdentry, &current->fs->root.mnt->mnt_root->d_subdirs, d_child) {
 		if (curdentry->d_flags & DCACHE_MOUNTED) {
-			//printk(KERN_INFO "JE ME FAIS BAISER");
 			(*number_of_entrys)++;
-			current_size = strlen(curdentry->d_name.name) + strlen(dentry_path_raw(curdentry, path, 1024)) + 3;
-			printk(KERN_INFO "%s have a length of %zu", curdentry->d_name.name, current_size);
+			current_size = strlen(curdentry->d_name.name) +
+			       	strlen(dentry_path_raw(curdentry, path, 1024)) + 3;
 			if (current_size > max_path_size)
 				max_path_size = current_size;
 		}
 	}
 	
-	printk(KERN_INFO "maxpath_size is %zu", max_path_size);
-	return max_path_size;
+	//Add 1 for / entry
+	(*number_of_entrys)++;
+	// + 1 for '\n'
+	return max_path_size + 1;
 }
 
 static ssize_t read_module(struct file *file, char __user *user, size_t len, loff_t *lofft) 
 {
-	/*struct dentry *curdentry;
-	char path[PATH_MAX];
-	char to_print[PATH_MAX];
+	struct dentry *curdentry;
+	char path[1024];
+	char buffer[1024];
+	size_t number_of_entrys = 0;
+	size_t max_length;
+	char *to_print;
+	int ret;
 
+	max_length = get_max_mount_size(&number_of_entrys);
+	to_print = kcalloc(number_of_entrys * max_length, sizeof(*to_print), GFP_KERNEL);
+	if (!to_print)
+		return 0;
 	curdentry = current->fs->root.dentry;
-	//snprintf(buffer, 1024, "%-*s%s\n", max, "root", curdentry->d_name.name);
-	printk(KERN_INFO "%s is mounted %s", curdentry->d_name.name, dentry_path_raw(curdentry, path, 200));
+	snprintf(buffer, 1024, "root   %s\n", curdentry->d_name.name);
+	strcat(to_print, buffer);
 	list_for_each_entry(curdentry, &current->fs->root.mnt->mnt_root->d_subdirs, d_child) {
 		if (curdentry->d_flags & DCACHE_MOUNTED) {
-			printk(KERN_INFO "%s is mounted %s", curdentry->d_name.name, dentry_path_raw(curdentry, path, PATH_MAX));
-			snprintf(user, PATH_MAX - 1, "%s %s\n", curdentry->d_name.name, dentry_path_raw(curdentry, path, PATH_MAX));
+			snprintf(buffer, 1024, "%s   %s\n", curdentry->d_name.name,
+				       	dentry_path_raw(curdentry, path, 1024));
+			strcat(to_print, buffer);
 		}
 	}
-	return simple_read_from_buffer(user, len, lofft, );*/
-	//get_number_of_entrys();
-	size_t test = 0;
-	get_max_mount_size(&test);
-	printk(KERN_INFO "test = %zu", test);
-	return 0;
+	ret = simple_read_from_buffer(user, len, lofft, to_print, strlen(to_print));
+	kfree(to_print);
+	return ret;
 }
 
 int init_module(void)
