203d178c8120fb4cb81d9c53fc426785cbf42ff0
lubenard <lubenard@student42.fr>
2021-03-31 12:06:33 +0200

    Improved code quality by using simple_read_from_buffer in Assignement_05

--- Assignement_05/main.c
+++ Assignement_05/main.c
@@ -6,8 +6,17 @@
 
 MODULE_LICENSE("GPL");
 
-ssize_t device_read(struct file *file, char *user, size_t size, loff_t *lofft); 
-ssize_t device_write(struct file *file, const char *user, size_t size, loff_t *lofft);
+ssize_t device_read(struct file *file, char *user, size_t size, loff_t *lofft) {
+	return (simple_read_from_buffer(user, 8, lofft, "lubenard", 8));
+}
+
+ssize_t device_write(struct file *file, const char *user, size_t size, loff_t *lofft) {
+	printk(KERN_INFO "Try to write to the kernel space string: '%s'", user);
+	if (strncmp(user, "lubenard", size) != 0)
+		return (-EINVAL);
+	printk(KERN_INFO "The user input is good, copying it into kernel");
+	return (size);
+}
 
 struct file_operations my_fops = {
 	.read = device_read,
@@ -20,38 +29,6 @@ struct miscdevice my_misc_driver= {
 	.fops = &my_fops,
 };
 
-ssize_t device_read(struct file *file, char *user, size_t size, loff_t *lofft) {
-	// We are forced to use a array, or it might cause buffer overflow.
-	static char character[8] = "lubenard";
-	static int chars_remaining = 1;
-	int ret;
-
-	ret = copy_to_user(user, character, 8);
-	if (ret)
-		return (-EFAULT);
-	/*
-	 * This sound like weird code, but prevent infinite loop printing of lubenard
-	 * Returning size of string print the characters, then return 0 saying there is no more to
-	 * read.
-	 * Line 43 is a reset for next time we want to read the file
-	 */
-	if (chars_remaining) {
-		chars_remaining = 0;
-		return (8);
-	} else {
-		chars_remaining = 1;
-		return (0);
-	}
-}
-
-ssize_t device_write(struct file *file, const char *user, size_t size, loff_t *lofft) {
-	printk(KERN_INFO "Try to write to the file, user = '%s'", user);
-	if (strncmp(user, "lubenard", size) != 0)
-		return (-EINVAL);
-	printk(KERN_INFO "The user input is good, copying it into kernel");
-	return (size);
-}
-
 int init_module(void)
 {
 	int retval;
@@ -59,7 +36,7 @@ int init_module(void)
 	printk(KERN_INFO "Module misc char device driver loaded\n");
 	if ((retval = misc_register(&my_misc_driver))) {
 		printk(KERN_INFO "Error while registering misc char device");
-		return retval;
+		return (retval);
 	}
 	return (0);
 }
